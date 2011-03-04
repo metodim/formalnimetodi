1578ead4dc3bc69b66939b889f1bc1bd5ba41123
carevski@gmail.com <carevski@gmail.com@971292e8-e873-053c-8555-0d2b08c23dd6>
2011-03-04 17:23:54 +0000

    -groovy script for executable jar

--- src/ii/edu/mk/gui/Parameters.java
+++ src/ii/edu/mk/gui/Parameters.java
@@ -10,7 +10,7 @@ public class Parameters<T> {
 
 	private static final String s = File.separator;
 	
-	public static final Parameters<String> RESOURCE_PATH =  new Parameters<String>("ii"+s+"edu"+s+"mk"+s+"resources"+s);
+	public static final Parameters<String> RESOURCE_PATH =  new Parameters<String>("ii"+s+"resources"+s);
 	public static final Parameters<String> APP_NAME =  new Parameters<String>("CCS");
 	public static final Parameters<Dimension> SCREEN_RESOLUTION = new Parameters<Dimension>(new Dimension(800, 600));
 	
