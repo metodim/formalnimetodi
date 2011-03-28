a540c49726d7bf8ea0b8eff4587d6865b62c95b5
dragan.sahpaskix <dragan.sahpaskix@971292e8-e873-053c-8555-0d2b08c23dd6>
2011-03-28 13:03:00 +0000

    - Small changes in the grammar - Adding domain objects and an ASTDomainBuilder which builds the domain tree from the ANTLR3 AST - Tests for the ASTDomainBuilder

---
+++ test/ii/edu/mk/parser/TestASTDomainBuilder.java
@@ -0,0 +1,58 @@
+package ii.edu.mk.parser;
+
+//import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import ii.edu.mk.ccs.domain.CcsAdd;
+import ii.edu.mk.ccs.domain.CcsProcess;
+import ii.edu.mk.ccs.domain.CcsRestrict;
+import ii.edu.mk.ccs.domain.CcsTrans;
+import ii.edu.mk.ccs.domain.base.CcsOperation;
+import ii.edu.mk.ccs.domain.base.OperatorType;
+
+import org.junit.Test;
+
+public class TestASTDomainBuilder extends BaseTest {
+
+	@Test
+	public void test1() throws Exception {
+
+		CcsOperation root = new ASTDomainBuilder().getRoot("a.B");
+		assertEquals(OperatorType.TRANSITION, root.getType());
+
+		assertTrue(root instanceof CcsTrans);
+		assertEquals("a", ((CcsTrans) root).getAction().getName());
+
+		assertEquals(OperatorType.PROCESS, ((CcsTrans) root).getRight()
+				.getType());
+		assertTrue(((CcsTrans) root).getRight() instanceof CcsProcess);
+		assertEquals("B", ((CcsTrans) root).getRight().getName());
+	}
+
+	@Test
+	public void test2() throws Exception {
+
+		CcsOperation root = new ASTDomainBuilder().getRoot("(a.B+b.C)\\{a}");
+		assertEquals(root.getType(), OperatorType.RESTRICTION);
+
+		assertTrue(root instanceof CcsRestrict);
+		assertEquals(1, ((CcsRestrict) root).getRestrictedActions().size());
+		assertEquals("a", ((CcsRestrict) root).getRestrictedActions().get(0)
+				.getName());
+
+		assertTrue(((CcsRestrict) root).getOperand() instanceof CcsAdd);
+		CcsAdd add = (CcsAdd) ((CcsRestrict) root).getOperand();
+		assertTrue(add.getLeft() instanceof CcsTrans);
+		assertTrue(add.getRight() instanceof CcsTrans);
+		CcsTrans left = (CcsTrans) add.getLeft();
+		CcsTrans right = (CcsTrans) add.getRight();
+
+		assertEquals("a", left.getAction().getName());
+		assertEquals("B", left.getRight().getName());
+		assertTrue(left.getRight() instanceof CcsProcess);
+
+		assertEquals("b", right.getAction().getName());
+		assertEquals("C", right.getRight().getName());
+		assertTrue(right.getRight() instanceof CcsProcess);
+	}
+}
