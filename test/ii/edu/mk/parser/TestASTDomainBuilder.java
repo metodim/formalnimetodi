29c337dc726446591b1cd90e6120e4029eb79cd4
carevski <carevski@971292e8-e873-053c-8555-0d2b08c23dd6>
2011-04-12 21:59:48 +0000

    -removed recursion in algorithm for building domain model from ast tree

--- test/ii/edu/mk/parser/TestASTDomainBuilder.java
+++ test/ii/edu/mk/parser/TestASTDomainBuilder.java
@@ -14,7 +14,7 @@ import org.junit.Test;
 public class TestASTDomainBuilder extends BaseTest {
 
 	@Test
-	public void test1() throws Exception {
+	public void testRecursive1() throws Exception {
 
 		CcsOperation root = ASTDomainBuilder.INSTANCE.getRoot("a.B");
 		assertEquals(OperatorType.TRANSITION, root.getType());
@@ -22,22 +22,60 @@ public class TestASTDomainBuilder extends BaseTest {
 		assertTrue(root instanceof CcsTrans);
 		assertEquals("a", ((CcsTrans) root).getAction().getName());
 
-		assertEquals(OperatorType.PROCESS, ((CcsTrans) root).getRight()
-				.getType());
+		assertEquals(OperatorType.PROCESS, ((CcsTrans) root).getRight().getType());
 		assertTrue(((CcsTrans) root).getRight() instanceof CcsProcess);
 		assertEquals("B", ((CcsTrans) root).getRight().getName());
 	}
 
 	@Test
-	public void test2() throws Exception {
+	public void testRecursive2() throws Exception {
 
 		CcsOperation root = ASTDomainBuilder.INSTANCE.getRoot("(a.B+b.C)\\{a}");
 		assertEquals(root.getType(), OperatorType.RESTRICTION);
 
 		assertTrue(root instanceof CcsRestrict);
 		assertEquals(1, ((CcsRestrict) root).getRestrictedActions().size());
-		assertEquals("a", ((CcsRestrict) root).getRestrictedActions().get(0)
-				.getName());
+		assertEquals("a", ((CcsRestrict) root).getRestrictedActions().get(0).getName());
+
+		assertTrue(((CcsRestrict) root).getOperand() instanceof CcsAdd);
+		CcsAdd add = (CcsAdd) ((CcsRestrict) root).getOperand();
+		assertTrue(add.getLeft() instanceof CcsTrans);
+		assertTrue(add.getRight() instanceof CcsTrans);
+		CcsTrans left = (CcsTrans) add.getLeft();
+		CcsTrans right = (CcsTrans) add.getRight();
+
+		assertEquals("a", left.getAction().getName());
+		assertEquals("B", left.getRight().getName());
+		assertTrue(left.getRight() instanceof CcsProcess);
+
+		assertEquals("b", right.getAction().getName());
+		assertEquals("C", right.getRight().getName());
+		assertTrue(right.getRight() instanceof CcsProcess);
+	}
+	
+	@Test
+	public void testNonRecursive1() throws Exception {
+
+		CcsOperation root = ASTDomainBuilder.INSTANCE.getRootNoRecursion("a.B");
+		assertEquals(OperatorType.TRANSITION, root.getType());
+
+		assertTrue(root instanceof CcsTrans);
+		assertEquals("a", ((CcsTrans) root).getAction().getName());
+
+		assertEquals(OperatorType.PROCESS, ((CcsTrans) root).getRight().getType());
+		assertTrue(((CcsTrans) root).getRight() instanceof CcsProcess);
+		assertEquals("B", ((CcsTrans) root).getRight().getName());
+	}
+	
+	@Test
+	public void testNonRecursive2() throws Exception {
+
+		CcsOperation root = ASTDomainBuilder.INSTANCE.getRootNoRecursion("(a.B+b.C)\\{a}");
+		assertEquals(root.getType(), OperatorType.RESTRICTION);
+
+		assertTrue(root instanceof CcsRestrict);
+		assertEquals(1, ((CcsRestrict) root).getRestrictedActions().size());
+		assertEquals("a", ((CcsRestrict) root).getRestrictedActions().get(0).getName());
 
 		assertTrue(((CcsRestrict) root).getOperand() instanceof CcsAdd);
 		CcsAdd add = (CcsAdd) ((CcsRestrict) root).getOperand();
