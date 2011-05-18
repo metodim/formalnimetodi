24497385a22ca58f78e03e53a5bcec3666041ce5
maja.siljanoska@gmail.com <maja.siljanoska@gmail.com@971292e8-e873-053c-8555-0d2b08c23dd6>
2011-05-18 15:03:53 +0000

    Using english variable names for consistency, proper code indentation

--- trunk/src/ii/edu/mk/saturation/Saturation.java
+++ trunk/src/ii/edu/mk/saturation/Saturation.java
@@ -16,233 +16,192 @@ public class Saturation {
 	 * @author Petar Gjorchevski
 	 * @author Metodi Micev
 	 */
-	
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		String Tau = "#";       
-        // Ja citame datotekata, pocnuvajci od headerot
+		String tau = "#";       
+		// Ja citame datotekata, pocnuvajci od headerot
 
 		BufferedReader bufReader = new BufferedReader(new InputStreamReader(new FileInputStream("Aldebaran_Graf.txt")));
 		String line = bufReader.readLine();
-        if(line == null || !line.startsWith("des") || line.isEmpty())
+		if(line == null || !line.startsWith("des") || line.isEmpty())
 			throw new IllegalArgumentException("Illegal argument for aldebaran descriptor");
 		String lines[] = line.split(",+");
 		int numberOfStates = Integer.parseInt(lines[2].split("\\)")[0].replaceAll(" +", ""));
 
-        // Grafot e vsusnost mnozestvo od tranzicii,
-        // koe za ovaa potreba ke go podelime na disjunktni 2*BrojNaSostojbi podmnozestva
-        // Kade za sekoja sostojba kreirame dve mnozestva
-        //   - Site tranzicii koi zavrsuvaat vo dadenata sostojba so akcija Tau, i 
-        //   - Site tranzicii koi zavrsuvaat vo dadenata sostojba so akcija razlicna od Tau
+		// Grafot e vsusnost mnozestvo od tranzicii,
+		// koe za ovaa potreba ke go podelime na disjunktni 2*BrojNaSostojbi podmnozestva
+		// Kade za sekoja sostojba kreirame dve mnozestva
+		//   - Site tranzicii koi zavrsuvaat vo dadenata sostojba so akcija Tau, i 
+		//   - Site tranzicii koi zavrsuvaat vo dadenata sostojba so akcija razlicna od Tau
+
+		ArrayList<HashSet<Transition>> trEndTau = new ArrayList<HashSet<Transition>>();
+		ArrayList<HashSet<Transition>> trEndOthers = new ArrayList<HashSet<Transition>>();
+
+		for (int i = 0; i < numberOfStates; i++)
+		{
+			HashSet<Transition> setTau = new HashSet<Transition>();
+			trEndTau.add(setTau);
+			HashSet<Transition> setOthers = new HashSet<Transition>();
+			trEndOthers.add(setOthers);
+		}
+
+		while((line = bufReader.readLine()) != null)
+		{
+			Transition edge = new Transition();
+			edge=Transition.FromString(line);
+
+			if (edge.label.equals(tau))
+			{
+				trEndTau.get(edge.endState).add(edge);
+			} 
+			else 
+			{ 
+				trEndOthers.get(edge.endState).add(edge);
+			}
+		}
+		bufReader.close();
+
+		// Prv cekor : Refleksivnost na Tau
+
+		for (int i = 0; i < numberOfStates; i++)
+		{
+			Transition rb = new Transition(); 
+			rb.startState = i; 
+			rb.label = tau; 
+			rb.endState = i;
+			trEndTau.get(i).add(rb);
+		}
+
+		// Vtor cekor : Tranzitivnost na Tau
+
+		ArrayList<Transition> workerList = new ArrayList<Transition>();
+
+		for (int i = 0; i < numberOfStates; i++)
+		{
+			for (Transition transition : trEndTau.get(i)) 
+			{
+				workerList.add(transition);
+			}
+
+			int itemsCount = workerList.size();
+			int currentPos = 0;
+			while(currentPos < itemsCount)
+			{    
+				int through = workerList.get(currentPos).startState;
+				for (Transition transition : trEndTau.get(through))
+				{   
+					if (trEndTau.get(i).add(Transition.FromTriple(transition.startState, tau, i)))
+					{
+						workerList.add(Transition.FromTriple(transition.startState, tau, i));
+						itemsCount++;
+					}
+				}
+				currentPos++;
+			}
+			workerList.clear();
+		}
+
+		// Tret cekor : PaQ, Q#R -> PaR
+
+		for (int i = 0; i < numberOfStates; i++)
+		{
+			for (Transition transition : trEndOthers.get(i)) 
+			{
+				workerList.add(transition);
+			}
+
+			int itemsCount = workerList.size();
+			int currentPos = 0;
+			while(currentPos < itemsCount)
+			{    
+				int through = workerList.get(currentPos).startState;
+				String currentLabel = workerList.get(currentPos).label;
+				for (Transition transition : trEndTau.get(through))
+				{   
+					if (trEndOthers.get(i).add(Transition.FromTriple(transition.startState, currentLabel, i)))
+					{
+						workerList.add(Transition.FromTriple(transition.startState, currentLabel, i));
+						itemsCount++;
+					}
+				}
+				currentPos++;
+			}
+			workerList.clear();
+		}
+
+		// Cetvrt cekor : P#Q, QaR -> PaR
+		// Prvo ke gi stavime vo drugi mnozestva koi se "bucket"-i, ama po pocetniot element
+
+		ArrayList<HashSet<Transition>> trStartTau = new ArrayList<HashSet<Transition>>();
+		ArrayList<HashSet<Transition>> trStartOthers = new ArrayList<HashSet<Transition>>();
+
+		for (int i = 0; i < numberOfStates; i++)
+		{
+			HashSet<Transition> setTau = new HashSet<Transition>();
+			trStartTau.add(setTau);
+			HashSet<Transition> setOthers = new HashSet<Transition>();
+			trStartOthers.add(setOthers);
+		}
 		
-        ArrayList<HashSet<Transition>> TrEndTau = new ArrayList<HashSet<Transition>>();
-        ArrayList<HashSet<Transition>> TrEndDrugi = new ArrayList<HashSet<Transition>>();
+		for (int i = 0; i < numberOfStates; i++)
+		{
+			for (Transition transition : trEndTau.get(i)) 
+			{ 
+				trStartTau.get(transition.startState).add(transition); 
+			}
+			trEndTau.get(i).clear();
+			for (Transition transition : trEndOthers.get(i)) 
+			{ 
+				trStartOthers.get(transition.startState).add(transition); 
+			}
+			trEndOthers.get(i).clear();
+		}
+		
+		for (int i = 0; i < numberOfStates; i++)
+		{
+			for (Transition transition : trStartOthers.get(i)) 
+			{
+				workerList.add(transition);
+			}
+		
+			int itemsCount = workerList.size();
+			int currentPos = 0;
+			while(currentPos < itemsCount)
+			{    
+				int through = workerList.get(currentPos).endState;
+				String currentLabel = workerList.get(currentPos).label;
+				for (Transition transition : trStartTau.get(through))
+				{   
+					if (trStartOthers.get(i).add(Transition.FromTriple(i, currentLabel, transition.endState)))
+					{
+						workerList.add(Transition.FromTriple(i, currentLabel, transition.endState));
+						itemsCount++;
+					}
+				}
+				currentPos++;
+			}
+			workerList.clear();
+		}
 
-        for (int i = 0; i < numberOfStates; i++)
-        {
-        	HashSet<Transition> MnozestvoTau = new HashSet<Transition>();
-            TrEndTau.add(MnozestvoTau);
-            HashSet<Transition> MnozestvoDrugi = new HashSet<Transition>();
-            TrEndDrugi.add(MnozestvoDrugi);
-        }
+		int numberOfTransitions=0;
+		for (int i = 0; i < numberOfStates; i++)
+		{ 
+			numberOfTransitions = numberOfTransitions + trStartOthers.get(i).size() + trStartTau.get(i).size();
+		}
 
-        while((line = bufReader.readLine()) != null)
-        {
-        	Transition rebro = new Transition();
-        	rebro=Transition.FromString(line);
-        //    System.out.printf("%s %s %s %n", rebro.startState, rebro.label, rebro.endState);
-       
-            if (rebro.label.equals(Tau))
-            {
-            	TrEndTau.get(rebro.endState).add(rebro);
-
-            	//System.out.printf("tau %sT %s %s %s %n", Tau, rebro.startState, rebro.label, rebro.endState);
-            } 
-               else { TrEndDrugi.get(rebro.endState).add(rebro);
-               //System.out.printf("Bu %sT %s %s %s %n", Tau, rebro.startState, rebro.label, rebro.endState);
-               }
-        }
-        bufReader.close();
-        //  System.out.printf("-----%n");
-       
-        // Prv cekor : Refleksivnost na Tau
-
-        for (int i = 0; i < numberOfStates; i++)
-        {
-        	Transition rb = new Transition(); 
-        	rb.startState = i; 
-            rb.label = Tau; 
-            rb.endState = i;
-            //if (!TrEndTau.get(i).contains(rb))
-            TrEndTau.get(i).add(rb);
-        }
-        /* Debug
-        for (int i = 0; i < numberOfStates; i++){
-        	for (Transition transition : TrEndTau.get(i))
-        		System.out.printf("%s %s %s %s %n", i, transition.startState, transition.label, transition.endState);
-        
-        //for (int i = 0; i < numberOfStates; i++)
-        	for (Transition transition : TrEndDrugi.get(i))
-        		System.out.printf("%s %s %s %s %n", i, transition.startState, transition.label, transition.endState);
-        }
-        System.out.printf("-----%n");
-        */
-        
-        // Vtor cekor : Tranzitivnost na Tau
-
-        ArrayList<Transition> WorkerList = new ArrayList<Transition>();
-        
-        for (int i = 0; i < numberOfStates; i++)
-        {
-        	 for (Transition transition : TrEndTau.get(i)) 
-        	 {
-				WorkerList.add(transition);
-			 }
-        	 //for (Transition transition : WorkerList){
-        		 //System.out.printf("worker %s %s %s %s %n", i, transition.startState, transition.label, transition.endState);
-                  
-        	 //}
-        	 //System.out.printf("dotua-----%n");
-        	
-            int ItemsCount=WorkerList.size();
-            int CurrentPos = 0;
-            while(CurrentPos<ItemsCount)
-            {    
-            	//System.out.printf("while %s %s %s %n", i, CurrentPos, ItemsCount);
-            	   int preku = WorkerList.get(CurrentPos).startState;
-            	   //System.out.println(preku);
-            	//   if (preku!=i) 
-	            	   for (Transition transition : TrEndTau.get(preku))
-	                   {   
-	            		   //if ((transition.startState!=preku)&&(transition.startState!=i))
-	            		   //{
-	            		   if (TrEndTau.get(i).add(Transition.FromTriple(transition.startState, Tau, i)))
-	                       {
-	                    	   WorkerList.add(Transition.FromTriple(transition.startState, Tau, i));
-	                           ItemsCount++;
-	                           //System.out.printf("tau %s %s %s %s %n", i, transition.startState, transition.label, transition.endState);
-	                           //System.out.printf("rb %s %s %s %s %n", i, rb.startState, rb.label, rb.endState);
-	                       }
-	            		   //}
-					    }
-                    CurrentPos++;
-            }
-            WorkerList.clear();
-        }
-        //System.out.printf("Cekor 2zavr %n");
-        // console_Debug
-       /* for (int i = 0; i < numberOfStates; i++){
-        	for (Transition transition : TrEndTau.get(i))
-        		System.out.printf("%s %s %s %s %n", i, transition.startState, transition.label, transition.endState);
-        }*/
-        
-        // Tret cekor : PaQ, Q#R -> PaR
-        
-        for (int i = 0; i < numberOfStates; i++)
-        {
-        	 for (Transition transition : TrEndDrugi.get(i)) 
-        	 {
-				WorkerList.add(transition);
-			 }
-        	 //for (Transition transition : WorkerList){
-        		 //System.out.printf("worker %s %s %s %s %n", i, transition.startState, transition.label, transition.endState);
-        	 //}
-        	 //System.out.printf("dotua-----%n");
-            int ItemsCount=WorkerList.size();
-            int CurrentPos = 0;
-            while(CurrentPos<ItemsCount)
-            {    
-            	//System.out.printf("while %s %s %s %n", i, CurrentPos, ItemsCount);
-            	   int preku = WorkerList.get(CurrentPos).startState;
-            	   String CurLabel=WorkerList.get(CurrentPos).label;
-            	   //System.out.println(preku);
-            	  // if (preku!=i) 
-	            	   for (Transition transition : TrEndTau.get(preku))
-	                   {   
-	            		//   if (transition.startState!=preku)
-	            		  // {
-	            		   if (TrEndDrugi.get(i).add(Transition.FromTriple(transition.startState, CurLabel, i)))
-	                       {
-	                    	   WorkerList.add(Transition.FromTriple(transition.startState, CurLabel, i));
-	                           ItemsCount++;
-	                           //System.out.printf("drugidrugi %s %s %s %s %n", i, transition.startState, CurLabel, transition.endState);
-	                           //System.out.printf("rb %s %s %s %s %n", i, rb.startState, rb.label, rb.endState);
-	                       }
-	            		   //}
-	                   }
-            	   CurrentPos++;
-            }
-            WorkerList.clear();
-        }
-        //System.out.printf("Cekor 3zavr %n");
-        
-        // Cetvrt cekor : P#Q, QaR -> PaR
-        // Prvo ke gi stavime vo drugi mnozestva koi se "bucket"-i, ama po pocetniot element
-
-        ArrayList<HashSet<Transition>> TrStartTau = new ArrayList<HashSet<Transition>>();
-        ArrayList<HashSet<Transition>> TrStartDrugi = new ArrayList<HashSet<Transition>>();
-
-        for (int i = 0; i < numberOfStates; i++)
-        {
-        	HashSet<Transition> MnozestvoTau = new HashSet<Transition>();
-        	TrStartTau.add(MnozestvoTau);
-            HashSet<Transition> MnozestvoDrugi = new HashSet<Transition>();
-            TrStartDrugi.add(MnozestvoDrugi);
-        }
-        for (int i = 0; i < numberOfStates; i++)
-        {
-        	for (Transition transition : TrEndTau.get(i)) { TrStartTau.get(transition.startState).add(transition); }
-        	TrEndTau.get(i).clear();
-        	for (Transition transition : TrEndDrugi.get(i)) { TrStartDrugi.get(transition.startState).add(transition); }
-        	TrEndDrugi.get(i).clear();
-        }
-        for (int i = 0; i < numberOfStates; i++)
-        {
-        	 for (Transition transition : TrStartDrugi.get(i)) 
-        	 {
-				WorkerList.add(transition);
-			 }
-        	 //for (Transition transition : WorkerList){
-        		 //System.out.printf("worker %s %s %s %s %n", i, transition.startState, transition.label, transition.endState);
-                  
-        	 //}
-        	 //System.out.printf("dotua-----%n");
-        	
-            int ItemsCount=WorkerList.size();
-            int CurrentPos = 0;
-            while(CurrentPos<ItemsCount)
-            {    
-           	   int preku = WorkerList.get(CurrentPos).endState;
-            	   String CurLabel=WorkerList.get(CurrentPos).label;
-            	  // if (preku!=i) 
-	            	   for (Transition transition : TrStartTau.get(preku))
-	                   {   
-	            	//	   if (transition.endState!=preku)
-	            		  // {
-	            		   if (TrStartDrugi.get(i).add(Transition.FromTriple( i,CurLabel,transition.endState)))
-	                       {
-	                    	   WorkerList.add(Transition.FromTriple(i, CurLabel, transition.endState));
-	                           ItemsCount++;
-	                       }
-	            		//   }
-					    }
-                    CurrentPos++;
-            }
-            WorkerList.clear();
-        }
-        int numberOfTransitions=0;
-        for (int i = 0; i < numberOfStates; i++){ numberOfTransitions=numberOfTransitions+TrStartDrugi.get(i).size()+TrStartTau.get(i).size();}
-        
-        BufferedWriter bufWriter = new BufferedWriter(new FileWriter("Aldebaran_Graf_saturated.txt"));
+		BufferedWriter bufWriter = new BufferedWriter(new FileWriter("Aldebaran_Graf_saturated.txt"));
 		bufWriter.write(String.format("des (0, %s, %s)", numberOfTransitions,numberOfStates));
 		bufWriter.newLine();
+
+		for (int i = 0; i < numberOfStates; i++)
+		{   
+			for (Transition transition : trStartOthers.get(i))
+				bufWriter.write(String.format("(%s, %s, %s)%n", transition.startState, transition.label, transition.endState));
+			for (Transition transition : trStartTau.get(i))
+				bufWriter.write(String.format("(%s, %s, %s)%n", transition.startState, transition.label, transition.endState));
+		}
 		
-        for (int i = 0; i < numberOfStates; i++)
-        {   for (Transition transition : TrStartDrugi.get(i))
-        		bufWriter.write(String.format("(%s, %s, %s)%n", transition.startState, transition.label, transition.endState));
-        	for (Transition transition : TrStartTau.get(i))
-            	bufWriter.write(String.format("(%s, %s, %s)%n", transition.startState, transition.label, transition.endState));
-        }
 		bufWriter.flush();
 		bufWriter.close();
 	}
--- trunk/src/ii/edu/mk/saturation/Transition.java
+++ trunk/src/ii/edu/mk/saturation/Transition.java
@@ -1,57 +1,57 @@
 package ii.edu.mk.saturation;
 
 public class Transition {
-        public int startState;
-        public String label;
-        public int endState;
-        private volatile int hashCode = 0;
-        
-        public static Transition FromString(String line)
-        {
-        	 Transition rebro = new Transition();
-             
-             String[] comps = line.split(",+");
-             rebro.startState = Integer.parseInt(comps[0].split("\\(")[1].replaceAll(" +", ""));
-             rebro.endState = Integer.parseInt(comps[2].split("\\)")[0].replaceAll(" +", ""));
-             rebro.label = comps[1].replaceAll(" +", "");
-             return rebro;
- 			   ///return new AldebaranFileLine(start, comps[1].replaceAll(" +", ""), end);
-        }
-        public static Transition FromTriple(int ss, String l, int es)
-        { 
-        	Transition rebro = new Transition();
-        	rebro.startState=ss;
-        	rebro.label=l;
-        	rebro.endState=es;
-        	return rebro;
-        }
-     @Override
-    public boolean equals(Object obj) {
-    // TODO Auto-generated method stub
-    	 if ( this == obj)
-    	 {
-    		 return true;
-    	 }
-    	 if ( ! (obj instanceof Transition))
-    			 {
-    		 		return false;
-    			 }
-    	 Transition rebro = (Transition) obj;
-    	 return startState == rebro.startState && endState == rebro.endState && label.equals(rebro.label);
-    //return super.equals(arg0);
-    	 }
-    	     @Override 
-    	    public int hashCode() {
-    	    // TODO Auto-generated method stub
-    	    	 final int multiplier=23;
-    	    	 if (hashCode == 0)
-    	    	 {
-    	    		 int code = 133;
-    	    		 code = multiplier * code + startState;
-    	    		 code = multiplier * code + endState;
-    	    		 code = multiplier * code + label.hashCode();
-    	    		 hashCode = code;
-    	    	 }
-    	    return hashCode;
-    	    }
+	public int startState;
+	public String label;
+	public int endState;
+	private volatile int hashCode = 0;
+
+	public static Transition FromString(String line)
+	{
+		Transition edge = new Transition();
+		String[] comps = line.split(",+");
+		edge.startState = Integer.parseInt(comps[0].split("\\(")[1].replaceAll(" +", ""));
+		edge.endState = Integer.parseInt(comps[2].split("\\)")[0].replaceAll(" +", ""));
+		edge.label = comps[1].replaceAll(" +", "");
+		return edge;
+		///return new AldebaranFileLine(start, comps[1].replaceAll(" +", ""), end);
+	}
+	public static Transition FromTriple(int ss, String l, int es)
+	{ 
+		Transition edge = new Transition();
+		edge.startState=ss;
+		edge.label=l;
+		edge.endState=es;
+		return edge;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		if ( this == obj)
+		{
+			return true;
+		}
+		if ( ! (obj instanceof Transition))
+		{
+			return false;
+		}
+		Transition edge = (Transition) obj;
+		return startState == edge.startState && endState == edge.endState && label.equals(edge.label);
+	}
+	
+	@Override 
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		final int multiplier=23;
+		if (hashCode == 0)
+		{
+			int code = 133;
+			code = multiplier * code + startState;
+			code = multiplier * code + endState;
+			code = multiplier * code + label.hashCode();
+			hashCode = code;
+		}
+		return hashCode;
+	}
 }
