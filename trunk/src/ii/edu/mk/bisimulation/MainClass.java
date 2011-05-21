7db73181e545c42fb490ea3f2d6c987373d892c0
maja.siljanoska@gmail.com <maja.siljanoska@gmail.com@971292e8-e873-053c-8555-0d2b08c23dd6>
2011-05-21 18:01:51 +0000

    Modified the graph generation method to support testing examples from mcrl2

--- trunk/src/ii/edu/mk/bisimulation/MainClass.java
+++ trunk/src/ii/edu/mk/bisimulation/MainClass.java
@@ -2,6 +2,7 @@ package ii.edu.mk.bisimulation;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 
@@ -10,129 +11,54 @@ public class MainClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("\n####################################################");
-		System.out.println("#                                                  #");
-		System.out.println("# STRONG BISIMULATION MINIMISATION AND EQUIVALENCE #");
-		System.out.println("#                                                  #");
-		System.out.println("####################################################\n");
-
 		System.out.println("=====/ EXAMPLE 1 /=====\n");
 
 		System.out.println("Graph1:");
-		Graph graph1 = generateGraph("lts002.aut");
-		System.out.println(graph1);
+		Graph graph1 = generateGraph("trains.aut");
 
 		Graph graph11 = new Graph(graph1);
 		Graph graph12 = new Graph(graph1);
 
-		System.out.println("Graph2:");
-		Graph graph2 = generateGraph("lts007.aut");
-		System.out.println(graph2);
-
-		Graph graph21 = new Graph(graph2);
-		Graph graph22 = new Graph(graph2);
-
 		System.out.println("\n1) Minimisation using naive bisimulation algorithm\n");
 
+		long start1 = System.currentTimeMillis();
 		ListPairProcess L1 = graph11.findStrongBisimulationNaive();
+		long end1 = System.currentTimeMillis();
+		System.out.println("Execution time: " + (end1-start1) + " ms");
 		System.out.println("Pairs of bisimilar states in Graph1: " + L1);
 		System.out.println();
+		
 		graph11.minimizationGraph(L1);
-		System.out.println("Minimised Graph1:");
-		System.out.println(graph11);
+		System.out.println("Minimal graph has " + graph11.getNumberOfStates() + " states");
+		System.out.println("Minimal graph has " + graph11.getNumberOfTransitions() + " transitions");
+	
+		//System.out.println("Minimised Graph1:");
+		//System.out.println(graph11);
 
-		ListPairProcess L2 = graph21.findStrongBisimulationNaive();
-		System.out.println("Pairs of bisimilar states in Graph2: " + L2);
-		System.out.println();
-		graph21.minimizationGraph(L2);
-		System.out.println("Minimised Graph2:");
-		System.out.println(graph21);
-
-		System.out.println("Are the two graphs strongly bisimilar?");
-		System.out.println(graph11.equalGraph(graph11.getInitialNode(), graph21, graph21.getInitialNode()));
+		//System.out.println("Are the two graphs strongly bisimilar?");
+		//System.out.println(graph11.equalGraph(graph11.getInitialNode(), graph21, graph21.getInitialNode()));
 
 		System.out.println("\n\n2) Minimisation using Fernandez bisimulation algorithm\n");
-
+		
+		//System.out.println(graph12);
+		long start2 = System.currentTimeMillis();
 		Partition P1 = graph12.findStrongBisimulationFernandez();
+		long end2 = System.currentTimeMillis();
+		System.out.println("Execution time: " + (end2-start2) + " ms");
 		System.out.println("Sets of mutually bisimilar states in Graph1: " + P1);
 		System.out.println();
+		
 		graph12.minimizationGraph(P1);
-		System.out.println("Minimised Graph1:");
-		System.out.println(graph12);
-
-		Partition P2 = graph22.findStrongBisimulationFernandez();
-		System.out.println("Sets of mutually bisimilar states in Graph2: " + P2);
+		System.out.println("Minimal graph has " + graph12.getNumberOfStates() + " states");
+		System.out.println("Minimal graph has " + graph12.getNumberOfTransitions() + " transitions");
+	
 		System.out.println();
-		graph22.minimizationGraph(P2);
-		System.out.println("Minimised Graph2:");
-		System.out.println(graph22);
-
-		System.out.println("Are the two graphs strongly bisimilar?");
-		System.out.println(graph12.equalGraph(graph12.getInitialNode(), graph22, graph22.getInitialNode()));
-
-		System.out.println("\n***********************\n");
-
-		System.out.println("=====/ EXAMPLE 2 /=====\n");
-
-		System.out.println("Graph3:");
-		Graph graph3 = generateGraph("lts006.aut");
-		System.out.println(graph3);
-
-		Graph graph31 = new Graph(graph3);
-		Graph graph32 = new Graph(graph3);
-
-		System.out.println("Graph4:");
-		Graph graph4 = generateGraph("lts008.aut");
-		System.out.println(graph4);
-
-		Graph graph41 = new Graph(graph4);
-		Graph graph42 = new Graph(graph4);
-
-		System.out.println("\n1) Minimisation using naive bisimulation algorithm\n");
-
-		ListPairProcess L3 = graph31.findStrongBisimulationNaive();
-		System.out.println("Pairs of bisimilar states in Graph3: " + L3);
-		System.out.println();
-		graph31.minimizationGraph(L3);
-		System.out.println("Minimised Graph3:");
-		System.out.println(graph31);
-
-		ListPairProcess L4 = graph41.findStrongBisimulationNaive();
-		System.out.println("Pairs of bisimilar states in Graph4: " + L4);
-		System.out.println();
-		graph41.minimizationGraph(L4);
-		System.out.println("Minimised Graph4:");
-		System.out.println(graph41);
-
-		System.out.println("Are the two graphs strongly bisimilar?");
-		System.out.println(graph31.equalGraph(graph31.getInitialNode(), graph41, graph41.getInitialNode()));
-
-		System.out.println("\n\n2) Minimisation using Fernandez bisimulation algorithm\n");
-
-		Partition P3 = graph32.findStrongBisimulationFernandez();
-		System.out.println("Sets of mutually bisimilar states in Graph3: " + P3);
-		System.out.println();
-		graph32.minimizationGraph(P3);
-		System.out.println("Minimised Graph3:");
-		System.out.println(graph32);
-
-		Partition P4 = graph42.findStrongBisimulationFernandez();
-		System.out.println("Sets of mutually bisimilar states in Graph4: " + P4);
-		System.out.println();
-		graph42.minimizationGraph(P4);
-		System.out.println("Minimised Graph4:");
-		System.out.println(graph42);
-
-		System.out.println("Are the two graphs strongly bisimilar?");
-		System.out.println(graph32.equalGraph(graph32.getInitialNode(), graph42, graph42.getInitialNode()));
-
-		System.out.println("\n***********************\n");
 
 	}
 
 	//should only be used for testing purposes
 	public static Graph generateGraph(String filename) {
-		return generateGraphFromFile(new File("test/ii/resources/"+filename));
+		return generateGraphFromFile(new File("test/ii/resources/analysis/"+filename));
 	}
 	
 	public static Graph generateGraphFromFile(File file){
@@ -143,7 +69,7 @@ public class MainClass {
 			String descriptor = scanner.nextLine();
 			String des = descriptor.substring(descriptor.indexOf('(') + 1, descriptor.indexOf(')'));
 
-			String[] desElements = des.split(", ");
+			String[] desElements = des.split(",");
 			String start = desElements[0];
 			int numberTransitions = Integer.parseInt(desElements[1]);
 			int numberStates = Integer.parseInt(desElements[2]);
@@ -154,12 +80,12 @@ public class MainClass {
 
 			for (int i = 1; i <= numberTransitions; i++) {
 				String edge = scanner.nextLine();
-				String ed = edge.substring(edge.indexOf('(') + 1, edge.indexOf(')'));
+				String ed = edge.substring(1, edge.length()-1);
 
-				String[] edgeElements = ed.split(", ");
-				String node1 = edgeElements[0];
+				String[] edgeElements = ed.split("\"");
+				String node1 = edgeElements[0].substring(0, edgeElements[0].length()-1);
 				String action = edgeElements[1];
-				String node2 = edgeElements[2];
+				String node2 = edgeElements[2].substring(1, edgeElements[2].length());
 
 				graph.addAction(action);
 
