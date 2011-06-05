c1ea4863e17ca81318f110674519b99e9994787f
carevski@gmail.com <carevski@gmail.com@971292e8-e873-053c-8555-0d2b08c23dd6>
2011-06-05 21:42:40 +0000

    - removing joda time usage - guarding against non loaded images, due to windows difficulty

--- trunk/test/ii/edu/mk/dev/Builder.groovy
+++ trunk/test/ii/edu/mk/dev/Builder.groovy
@@ -39,7 +39,7 @@ class Builder {
 	
 	void defineClientJars(){
 		jars =  ["swingx-core-1.6.2.jar",
-				"joda-time-1.6.2.jar",
+//				"joda-time-1.6.2.jar",
 				"miglayout-3.7.4-swing.jar",
 				"log4j-1.2.16.jar",
 				"antlr-runtime-3.3.jar",
